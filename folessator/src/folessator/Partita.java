f2b123fcb9a0861d9ad027c69e97debf46ccc8ec
Danikzor <daniele.foti@studio.unibo.it>
2017-10-25 16:38:24 +0200

    ho creato la classep artita

--- folessator/src/folessator/Partita.java
+++ folessator/src/folessator/Partita.java
@@ -1,29 +1,38 @@
 package folessator;
 
-import org.apache.jena.query.*;
-import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+import java.util.HashMap;
+import java.util.Map;
 
-public class FolessatorMain {
-    static public void main(String...argv) {
-        String queryStr = "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"
-        		+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-        		+ "select ?URI ?categoria "
-        		+ "where {"
-        		+ "?URI rdf:type foaf:Person."
-        		+ "?URI rdf:type ?categoria. }"
-        		+ "";
-        Query query = QueryFactory.create(queryStr);
-        // Remote execution.
-        try ( QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query) ) {
-            // Set the DBpedia specific timeout.
-           	((QueryEngineHTTP)qexec).addParam("timeout", "1000000") ;
+import folessator.Partita.Risposta;
 
-
-            // Execute.
-            ResultSet rs = qexec.execSelect();
-            ResultSetFormatter.out(System.out, rs, query);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+public class Partita {
+	
+	
+	public enum Risposta {
+		SI, NO, FORSE;
+		@Override
+		public String toString() {
+			   return super.toString().toLowerCase();
+		}
+	}
+	
+	private static Map<Object, Risposta> stato = new HashMap<>();
+	
+	public void add_risposta(Object cluster, Risposta risposta) {
+		stato.put(cluster, risposta);
+		}
+	public Risposta getRisposta(Object cluster) {
+		 return stato.get(cluster);
+		 }
+	public void change_risposta(Object cluster, Risposta risposta) {
+		stato.replace(cluster, risposta);
+	}
+	public int numeroDomande() {
+		return stato.size();
+	}
+	public HashMap<Object, Risposta> getStorico() {
+		return new HashMap<Object,Risposta>(stato);
+	}
+	
+	
 }
