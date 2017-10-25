bc111181954dd63f0530770cb9bfd1412423f88f
Danikzor <daniele.foti@studio.unibo.it>
2017-10-25 17:36:45 +0200

    rename delle variabili

--- folessator/src/folessator/Partita.java
+++ folessator/src/folessator/Partita.java
@@ -3,35 +3,34 @@ package folessator;
 import java.util.HashMap;
 import java.util.Map;
 
-import folessator.Partita.Risposta;
 
 public class Partita {
 	
 	
-	public enum Risposta {
-		SI, NO, FORSE;
+	public enum Answer {
+		YES, NO, MAYBE;
 		@Override
 		public String toString() {
 			   return super.toString().toLowerCase();
 		}
 	}
 	
-	private static Map<Object, Risposta> stato = new HashMap<>();
+	private static Map<Object, Answer> stato = new HashMap<>();
 	
-	public void addRisposta(Object cluster, Risposta risposta) {
+	public void addRisposta(Object cluster, Answer risposta) {
 		stato.put(cluster, risposta);
 		}
-	public Risposta getRisposta(Object cluster) {
+	public Answer getRisposta(Object cluster) {
 		 return stato.get(cluster);
 		 }
-	public void changeRisposta(Object cluster, Risposta risposta) {
+	public void changeAnswer(Object cluster, Answer risposta) {
 		stato.replace(cluster, risposta);
 	}
 	public int numeroDomande() {
 		return stato.size();
 	}
-	public HashMap<Object, Risposta> getStorico() {
-		return new HashMap<Object,Risposta>(stato);
+	public HashMap<Object, Answer> getStorico() {
+		return new HashMap<Object,Answer>(stato);
 	}
 	
 	
