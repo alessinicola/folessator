164d856e15d2826fcbbfb015ce47a694d6a853d4
Danikzor <daniele.foti@studio.unibo.it>
2017-10-28 01:37:09 +0200

    aggiunto secondo main per riempire database

--- folessator/src/folessator/QuestionDatabaseITA.java
+++ folessator/src/folessator/QuestionDatabaseITA.java
@@ -2,16 +2,28 @@ package folessator;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Set;
 
 public class QuestionDatabaseITA implements Serializable, QuestionDatabase {
 
 	private static final long serialVersionUID = 1L;
-	private HashMap<String,String> questionMap;
+	private HashMap<String,String> questionMap = new HashMap<String, String>();
 	
 	
 	@Override
-	public String getQuestion(String topic) {
-		return topic+"trololol";
+	public  String getQuestion(String topic) {
+		return questionMap.get(topic);
+	}
+	
+	public void stampa() {
+		for (String topic : questionMap.keySet()) {
+			System.out.println("" + topic  + " " + questionMap.get(topic));
+			
+		}
+	}
+
+	public void putQuestion(String topic, String question) {
+		questionMap.put(topic, question);
 	}
 	
 
